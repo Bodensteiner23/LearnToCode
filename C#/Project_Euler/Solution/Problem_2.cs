2727916ac9c5898965a08b5851118dd2326a102e
Matthias Bodensteiner <matthias.bodensteiner@fau.de>
2023-12-17 20:08:38 +0100

    added Problem_2.cs file

---
+++ C#/Project_Euler/Solution/Problem_2.cs
@@ -0,0 +1,12 @@
+namespace Project_Euler.Solution
+{
+    public class Problem_2
+    {
+        /* ========================== Variables ============================ */
+
+        /* ======================= Private Functions ======================= */
+
+
+
+    }
+}

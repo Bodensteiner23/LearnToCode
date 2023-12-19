a01c6a9ed37e53f1a5ee8a4297e08bbd2b38f7c8
Matthias Bodensteiner <matthias.bodensteiner@fau.de>
2023-12-19 21:36:05 +0100

    Finished Problem 5!

--- C#/Project_Euler/Program.cs
+++ C#/Project_Euler/Program.cs
@@ -9,36 +9,37 @@
             Solution.Problem_2 pb2 = new Solution.Problem_2();
             Solution.Problem_3 pb3 = new Solution.Problem_3();
             Solution.Problem_4 pb4 = new Solution.Problem_4();
+            Solution.Problem_5 pb5 = new Solution.Problem_5();
 
             //Ask the user which problem to solve
             System.Console.Write("Enter the problem number to solve: ");
-            string _problem = System.Console.ReadLine();
-            long _solvedProblem = 0;
+            string problem = System.Console.ReadLine();
+            long solvedProblem = 0;
 
 
-            switch (_problem)
+            switch (problem)
             {
                 case "Problem 1":
-                    _solvedProblem = pb1.Calculate_Sum();
+                    solvedProblem = pb1.Calculate_Sum();
                     break;
                 case "Problem 2":
-                    _solvedProblem = pb2.Calculate_Fibbonaci_Sum();
+                    solvedProblem = pb2.Calculate_Fibbonaci_Sum();
                     break;
                 case "Problem 3":
-                    _solvedProblem = pb3.LargestPrimeFactor();
+                    solvedProblem = pb3.LargestPrimeFactor();
                     break;
                 case "Problem 4":
-                    _solvedProblem = pb4.FindLargestPalindromeProduct();
+                    solvedProblem = pb4.FindLargestPalindromeProduct();
                     break;
                 case "Problem 5":
-                    
+                    solvedProblem = pb5.FindSmallestMultiple();
                     break;
                 default:
                     System.Console.WriteLine("Invalid input. Please enter valid Problem. For example: Problem 1");
                     break;
             }
 
-            System.Console.WriteLine("Result " + _problem + ": " + _solvedProblem);
+            System.Console.WriteLine("Result " + problem + ": " + solvedProblem);
 
 
 
--- C#/Project_Euler/Solution/Problem_5.cs
+++ C#/Project_Euler/Solution/Problem_5.cs
@@ -2,10 +2,30 @@ namespace Project_Euler.Solution
 {
     public class Problem_5
     {
-        /* ========================== Variables ============================ */
-
         /* ======================= Public Functions ======================== */
+        public long FindSmallestMultiple()
+        {
 
-        /* ======================= Private Functions ======================= */
+            long numberToCheck = 1;
+            int divider = 1;
+            
+            // Check if the number is divisible by all numbers from 1 to 20
+            while (divider <= 20)
+            {
+                // If the number is divisible by the divider, increment the divider 
+                if (numberToCheck % divider == 0)
+                {
+                    divider++;
+                }
+                // If the number is not divisible by the divider, reset the divider and increment the number
+                else
+                {
+                    numberToCheck++;
+                    divider = 1;
+                }
+            }
+
+            return numberToCheck;
+        }
     }
 }

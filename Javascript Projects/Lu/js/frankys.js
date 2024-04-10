790bc278bf3fb222b1ae9f80f6a1de395ef80335
Matthias Bodensteiner <matthias.bodensteiner@fauad.fau.de>
2024-04-11 00:40:37 +0200

    First Version of Table. Needs to be worked on.

--- Javascript Projects/Lu/js/frankys.js
+++ Javascript Projects/Lu/js/frankys.js
@@ -1,6 +1,7 @@
 /* ============================= Variables ============================ */
 const playerCountButton = document.getElementById("player_count")
 const heading = document.getElementById("player_count_heading")
+// const gameTable = document.getElementById("game_table")
 
 
 /* ============================= Functions ============================ */
@@ -38,28 +39,62 @@ function getPlayerCount(_playerCount) {
 }
 
 
-
-
-
-
-
 function createTable(row, col) {
-    let body = document.body
-    let tbl = document.createElement("table")
-    tbl.style.width = "200px"
-    tbl.style.border = "1px solid black"
+    const tbl = document.createElement("table")
+    tbl.id = "table"
+    const tblBody = document.createElement("tbody")
+    tblBody.id = "tableBody"
 
-    for (let i = 0; i < row; i++){
-        let tr = tbl.insertRow()
-        for (let j = 0; i < col; i++) {
-            let td = tr.insertCell()
-            td.appendChild(document.createTextNode("${i}, ${j}"))
-            td.style.border = "1px solid black"
+
+    // Creating cells
+    for (let i = 0; i < row; i++) {
+        const row = document.createElement("tr")
+        row.id = "tableRow"
+
+        for (let j = 0; j < col; j++) {
+            const cell = document.createElement("td")
+            cell.id = "tableCell"
+
+            const cellText = document.createTextNode("Test" + i)
+            cell.appendChild(cellText)
+            row.appendChild(cell)
+
         }
+        tblBody.appendChild(row)
+
     }
-    body.appendChild(tbl)
+
+    tbl.appendChild(tblBody)
+    document.body.appendChild(tbl)
 
 }
 
+createTable(9, 3)
 
-createTable(3, 5)
+
+
+
+
+// function createTable(row, col) {
+//     let body = document.body
+//     let tbl = document.getElementById("game_table")
+//     if (tbl) {
+//         tbl.style.width = "200px"
+//         tbl.style.border = "1px solid black"
+
+//     }
+
+//     for (let i = 0; i < row; i++){
+//         let tr = tbl.insertRow()
+//         for (let j = 0; i < col; i++) {
+//             let td = tr.insertCell()
+//             td.appendChild(document.createTextNode("${i}, ${j}"))
+//             td.style.border = "1px solid black"
+//         }
+//     }
+//     body.appendChild(tbl)
+
+// }
+
+
+// createTable(3, 5)

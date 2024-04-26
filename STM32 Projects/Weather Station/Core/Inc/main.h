2af7877a11b8e1edaec0e99f1212cdc2e1e948c7
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-04-26 12:52:29 +0200

    Working Version of using ms timer.

--- STM32 Projects/Weather Station/Core/Inc/main.h
+++ STM32 Projects/Weather Station/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define PWM_Pin_Pin GPIO_PIN_8
+#define PWM_Pin_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 

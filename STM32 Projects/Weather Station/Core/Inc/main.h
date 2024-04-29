cc6d01aef8cd602cd9bf93e518dfbc038a61a77d
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-04-29 23:48:01 +0200

    Working Version of Interrupts and sending them via Uart.

--- STM32 Projects/Weather Station/Core/Inc/main.h
+++ STM32 Projects/Weather Station/Core/Inc/main.h
@@ -47,7 +47,7 @@ extern "C" {
 /* Exported macro ------------------------------------------------------------*/
 /* USER CODE BEGIN EM */
 void us_delay(uint16_t us);
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
+//void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
--- STM32 Projects/Weather Station/Core/Src/main.c
+++ STM32 Projects/Weather Station/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "dht11.h"
+#include <string.h>
 
 /* USER CODE END Includes */
 
@@ -43,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
 
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
@@ -53,7 +54,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -65,6 +66,9 @@ void us_delay(uint16_t us) {
 	while ((uint16_t)__HAL_TIM_GET_COUNTER(&htim1) < us);
 }
 
+
+
+uint8_t tx_buff[]={0,1,2,3,4,5,6,7,8,9};
 
 /* USER CODE END 0 */
 
@@ -97,30 +101,22 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
-  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim1);
 
-  uint8_t data = 20;
 
-  uint8_t txBuffer[1];
-
-  void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
-  {
-	  txBuffer[0] = data;
-	  HAL_UART_Transmit_IT(&huart2, txBuffer, sizeof (txBuffer));
-  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_UART_Transmit_IT(&huart1, tx_buff, sizeof(tx_buff));
+	  HAL_Delay(200);
 //	  dht11_initCommunication();
 //	  HAL_Delay(1000);
 
-	  HAL_UART_Transmit_IT(&huart2, txBuffer, sizeof (txBuffer));
-	  HAL_Delay(2000);
 
 
     /* USER CODE END WHILE */
@@ -223,35 +219,35 @@ static void MX_TIM1_Init(void)
 }
 
 /**
-  * @brief USART2 Initialization Function
+  * @brief USART1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_USART2_UART_Init(void)
+static void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+  /* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+  /* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+  /* USER CODE END USART1_Init 2 */
 
 }
 

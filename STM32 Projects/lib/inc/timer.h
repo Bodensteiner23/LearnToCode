42f28d453282c307413f716f43b1c48254e2f8d4
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-03 01:03:36 +0200

    Started to implement own library.

---
+++ STM32 Projects/lib/inc/timer.h
@@ -0,0 +1,9 @@
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+#include <stdint.h>
+
+void timer_usDelay(uint16_t us);
+
+
+#endif /* INC_DHT11_H_ */
---
+++ STM32 Projects/lib/src/timer.c
@@ -0,0 +1,17 @@
+#include "timer.h"
+#include <stdint.h>
+#include <stm32f4xx_hal.h>
+
+
+TIM_HandleTypeDef *hhtim1;
+
+// ToDO: Add initTimer in main
+void timer_initTimer(TIM_HandleTypeDef *_htim) {
+	hhtim1 = _htim;
+}
+
+
+void timer_usDelay(uint16_t us) {
+	__HAL_TIM_SET_COUNTER(hhtim1, 0);
+	while ((uint16_t)__HAL_TIM_GET_COUNTER(hhtim1) < us);
+}

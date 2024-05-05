3d8e948f00e9cf648021c173662784724c70ccb6
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-05 17:26:33 +0200

    Check Response should be done.

--- STM32 Projects/Weather Station/Core/Src/dht11.c
+++ STM32 Projects/Weather Station/Core/Src/dht11.c
@@ -8,7 +8,6 @@
 #include "timer.h"
 #include "gpio.h"
 #include "hw_init.h"
-#include "main.h"
 #include <stdbool.h>
 
 gpio_t *hw_pins;
@@ -22,52 +21,36 @@ void hw_init_pins(gpio_t *_hw_pins) {
 void dht11_initCommunication(void) {
 
 	gpio_setPinOutput(hw_pins->dht11_port, hw_pins->dht11_pin);
-	HAL_GPIO_WritePin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin, SET);
+	HAL_GPIO_WritePin(hw_pins->dht11_port, hw_pins->dht11_pin, SET);
 
-	HAL_GPIO_WritePin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin, RESET);
+	HAL_GPIO_WritePin(hw_pins->dht11_port, hw_pins->dht11_pin, RESET);
 	timer_usDelay(18000);
-	HAL_GPIO_WritePin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin, SET);
-	timer_usDelay(30);
+	HAL_GPIO_WritePin(hw_pins->dht11_port, hw_pins->dht11_pin, SET);
+	timer_usDelay(50);
 
+	// Now sensor is pulling line LOW
 	gpio_setPinInput(hw_pins->dht11_port, hw_pins->dht11_pin);
 }
 
 
 bool dht11_checkResponse(void) {
-	uint8_t state = 0;
-
-	timer_usDelay(40);
-	state = HAL_GPIO_ReadPin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin);
-	if (!state) {
-		timer_usDelay(60);
-		state = HAL_GPIO_ReadPin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin);
-		if (state) {
-			// Response was ok
+	// Wait for Pin to go HIGH
+	while (!(HAL_GPIO_ReadPin(hw_pins->dht11_port, hw_pins->dht11_pin))) {
+		if (HAL_GPIO_ReadPin(hw_pins->dht11_port, hw_pins->dht11_pin)) {
 			return true;
 		}
 	}
-	// Communication not ok
 	return false;
 }
 
 
 void dht11_readBit(void) {
-	uint8_t state = 1;
-
-	// Wait until Data Line is pulled LOW
-	while (state) {
-		state = HAL_GPIO_ReadPin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin);
-	} while (!state) {
-		state = HAL_GPIO_ReadPin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin);
-
-	}
-	// Check if Data Line is high
-	timer_usDelay(100);
-	state = HAL_GPIO_ReadPin(DHT11_Sensor_GPIO_Port, DHT11_Sensor_Pin);
-
+	// Wait for Pin to go HIGH
 
 
 }
+
+
 
 
 

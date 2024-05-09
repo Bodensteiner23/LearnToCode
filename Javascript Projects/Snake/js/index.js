5d7bfa91155ee643325ed8a1c9c8e88c3dac23bc
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-09 21:19:05 +0200

    Snake is now being updatet with snake and step structs.

--- Javascript Projects/Snake/js/index.js
+++ Javascript Projects/Snake/js/index.js
@@ -1,19 +1,35 @@
-
-
 const field = document.getElementById("snake_map")
+let ctx
 
 let field_size = 25
 let field_x = 20
 let field_y = 20
 
+const step = {
+    x: 1,
+    y: 1
+}
+
+const snake = {
+    x: field_size * step.x,
+    y: field_size * step.y
+
+}
 
 function initField() {
     field.width = field_x * field_size
     field.height = field_y * field_size
     
-    const ctx = field.getContext("2d")
+    ctx = field.getContext("2d")
 }
 
 initField()
 
+function drawSnake() {
+    ctx.fillStyle = "lime"
+    ctx.fillRect(snake.x, snake.y, field_size, field_size)
 
+}
+
+
+drawSnake()

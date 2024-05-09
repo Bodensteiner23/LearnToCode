9f578eef4e6c30c737d3864a01be5ac57b65f628
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-09 20:56:38 +0200

    Working version of creating playing field.

--- Javascript Projects/Snake/js/index.js
+++ Javascript Projects/Snake/js/index.js
@@ -1,10 +1,19 @@
 
 
-const snakeMap = document.getElementById("snake_map")
+const field = document.getElementById("snake_map")
+
+let field_size = 25
+let field_x = 20
+let field_y = 20
 
 
-
-if (snakeMap) {
-    snakeMap.style.backgroundColor = "red"
+function initField() {
+    field.width = field_x * field_size
+    field.height = field_y * field_size
+    
+    const ctx = field.getContext("2d")
 }
+
+initField()
+
 

f1bbd450a62c8df4ce66db8128a71ee1720feb31
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-16 22:53:51 +0200

    Self test not working?

--- STM32 Projects/Active_Suspension_TestBoard/Core/Src/lis3dh.c
+++ STM32 Projects/Active_Suspension_TestBoard/Core/Src/lis3dh.c
@@ -1,16 +1,29 @@
 #include "lis3dh.h"
 #include "lis3dh_driver.h"
 #include <stdint.h>
+#include <stm32g4xx_hal.h>
 
 
 uint8_t dataBuffer[255];
 LIS3DH_Aux123Raw_t aux_raw_data;
 
 void lis3dh_initSensor(void) {
-	//CTRL_REG1
-	LIS3DH_WriteReg(LIS3DH_CTRL_REG1, 0b10010111);
+	//CTRL_REG2
+	LIS3DH_WriteReg(LIS3DH_CTRL_REG2, 0x0);
+	//CTRL_REG3
+	LIS3DH_WriteReg(LIS3DH_CTRL_REG3, 0x0);
 	//CTRL_REG4
-	LIS3DH_WriteReg(LIS3DH_CTRL_REG4, 0b00000100);
+	LIS3DH_WriteReg(LIS3DH_CTRL_REG4, 0x80);
+	//CTRL_REG1
+	LIS3DH_WriteReg(LIS3DH_CTRL_REG1, 0x47);
+
+	HAL_Delay(100);
+
+	//CTRL_REG4
+	LIS3DH_WriteReg(LIS3DH_CTRL_REG1, 0x82);
+
+	HAL_Delay(100);
+
 
 }
 

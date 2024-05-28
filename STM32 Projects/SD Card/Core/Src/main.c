99e2c21431094096acfa74def0a920c8a8865d3e
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-05-28 17:29:24 +0200

    No working. Trying another guide.

--- STM32 Projects/SD Card/Core/Src/main.c
+++ STM32 Projects/SD Card/Core/Src/main.c
@@ -19,7 +19,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "fatfs.h"
-
+#include "ff.h"
+#include <string.h>
+#include <stdio.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -60,7 +62,40 @@ static void MX_SPI2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+FATFS fs;  // file system
+FIL fil; // File
+FILINFO fno;
+FRESULT fresult;  // result
+UINT br, bw;  // File read/write count
 
+
+/**** capacity related *****/
+FATFS *pfs;
+DWORD fre_clust;
+uint32_t total, free_space;
+
+#define BUFFER_SIZE 128
+char buffer[BUFFER_SIZE];  // to store strings..
+
+int i=0;
+
+int bufsize (char *buf)
+{
+	int i=0;
+	while (*buf++ != '\0') i++;
+	return i;
+}
+
+void clear_buffer (void)
+{
+	for (int i=0; i<BUFFER_SIZE; i++) buffer[i] = '\0';
+}
+
+void send_uart (char *string)
+{
+	uint8_t len = strlen (string);
+	HAL_UART_Transmit(&huart2, (uint8_t *) string, len, HAL_MAX_DELAY);  // transmit in blocking mode
+}
 /* USER CODE END 0 */
 
 /**
@@ -96,6 +131,80 @@ int main(void)
   MX_SPI2_Init();
   MX_FATFS_Init();
   /* USER CODE BEGIN 2 */
+
+  HAL_Delay (500);
+
+  /*** SD Card ***/
+  fresult = f_mount(&fs, "/", 1);
+  if (fresult != FR_OK) send_uart ("ERROR!!! in mounting SD CARD...\n\n");
+  else send_uart("SD CARD mounted successfully...\n\n");
+
+
+  /*************** Card capacity details ********************/
+
+    	/* Check free space */
+    	f_getfree("", &fre_clust, &pfs);
+
+    	total = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
+    	sprintf (buffer, "SD CARD Total Size: \t%lu\n",total);
+    	send_uart(buffer);
+    	clear_buffer();
+    	free_space = (uint32_t)(fre_clust * pfs->csize * 0.5);
+    	sprintf (buffer, "SD CARD Free Space: \t%lu\n\n",free_space);
+    	send_uart(buffer);
+    	clear_buffer();
+
+
+
+      	/************* The following operation is using PUTS and GETS *********************/
+
+      	/* Open file to write/ create a file if it doesn't exist */
+          fresult = f_open(&fil, "file1.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
+
+      	/* Writing text */
+      	f_puts("This data is from the FILE1.txt. And it was written using ...f_puts... ", &fil);
+
+      	/* Close file */
+      	fresult = f_close(&fil);
+
+      	if (fresult == FR_OK)send_uart ("File1.txt created and the data is written \n");
+
+      	/* Open file to read */
+      	fresult = f_open(&fil, "file1.txt", FA_READ);
+
+      	/* Read string from the file */
+      	f_gets(buffer, f_size(&fil), &fil);
+
+      	send_uart("File1.txt is opened and it contains the data as shown below\n");
+      	send_uart(buffer);
+      	send_uart("\n\n");
+
+      	/* Close file */
+      	f_close(&fil);
+
+      	clear_buffer();
+
+
+
+      	/**************** The following operation is using f_write and f_read **************************/
+
+      	/* Create second file with read write access and open it */
+      	fresult = f_open(&fil, "file2.txt", FA_CREATE_ALWAYS | FA_WRITE);
+
+      	/* Writing text */
+      	strcpy (buffer, "This is File2.txt, written using ...f_write... and it says Hello from Controllerstech\n");
+
+      	fresult = f_write(&fil, buffer, bufsize(buffer), &bw);
+
+      	send_uart ("File2.txt created and data is written\n");
+
+      	/* Close file */
+      	f_close(&fil);
+
+
+
+
+
 
   /* USER CODE END 2 */
 

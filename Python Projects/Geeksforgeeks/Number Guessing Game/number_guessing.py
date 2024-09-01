0d25334e0c27d52628878b8d2b31ea0d44f893a1
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-01 15:18:23 +0200

    Also added try counter and winning screen.

--- Python Projects/Geeksforgeeks/Number Guessing Game/number_guessing.py
+++ Python Projects/Geeksforgeeks/Number Guessing Game/number_guessing.py
@@ -5,6 +5,7 @@
 # User selects Range from A to B
 # Random Number between A and B is chosen
 # User will iterate with inputs
+# Player has 5 trys to get the number
 
 
 import random
@@ -36,13 +37,42 @@ def generate_number_to_guess():
     return random_number
 
 
+def user_finds_number(number_to_guess):
+
+    correct_number = False
+    remaining_trys = 5
+    try_counter = 1
+    
+    print(f"Remaining trys: {remaining_trys}")
+
+    while (correct_number != True):
+        user_guess = int(input("Type your guess: "))
+
+        if remaining_trys == 1:
+            print(f"No more trys left. You lost! Correct Number was: {number_to_guess}")
+            break
+        elif user_guess > number_to_guess:
+            remaining_trys -= 1
+            try_counter += 1
+            print(f"Your number is too high.  Remaining trys: {remaining_trys}")
+        elif user_guess < number_to_guess:
+            remaining_trys -= 1
+            try_counter += 1
+            print(f"Your number is too low.   Remaining trys: {remaining_trys}")
+        else:
+            print("Correct number!!")
+            print(f"You needed {try_counter} trys!")
+            correct_number =True
+            
+
+
 
 
 
 def main():
     number_to_guess = generate_number_to_guess()
 
-    print(number_to_guess)
+    user_finds_number(number_to_guess)
 
 
 

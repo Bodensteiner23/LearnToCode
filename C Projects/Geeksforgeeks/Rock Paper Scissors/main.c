5d628c77f726518238f2f298a508d8a0c663ba21
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-02 17:05:00 +0200

    Made some Progress with gameloop.

--- C Projects/Geeksforgeeks/Rock Paper Scissors/main.c
+++ C Projects/Geeksforgeeks/Rock Paper Scissors/main.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "main.h"
 
 
+void gameLoop(void) {
+    char valid_inputs[] = {'r', 'p', 's'};
+    const char* game_inputs[] = {"Rock", "Paper", "Scissor"};
+
+    uint8_t points_player = 0;
+    uint8_t points_bot = 0;
+
+    while ((points_player != 3) & (points_bot != 3)) {
+
+        char player_turn = 0;
+        printf("Pick your symbol (r,p,s): ");
+        fflush(stdout);
+        player_turn = getchar();
+        getchar();      // Handle enter key input
+
+        if (validInput(player_turn, valid_inputs) != true) {
+            printf("Your input was not correct. Try again.\n");
+            fflush(stdout);
+            continue;
+        }
+
+        // Get random number of for bot turn
+        const char* bot_turn = game_inputs[rand() % 3];
+        printf("Bot picked: %s\n", bot_turn);
+        fflush(stdout);
+    }
+}   
 
 
+bool validInput(char _player_turn, char _valid_inputs[]) {
+
+    for (uint8_t i = 0; i < 3; i++) {
+        if (_player_turn == _valid_inputs[i]) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(void) {
 
-    uint8_t test = 3;
+    printf("##########################################\n"
+    "           Rock Paper Sciccors\n"
+    "##########################################\n");
+    fflush(stdout);
 
-    printf("hallo");
+    gameLoop();
+
 
     return 0;
 }
---
+++ C Projects/Geeksforgeeks/Rock Paper Scissors/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+bool validInput(char _user_input, char _valid_inputs[]);
+
+
+#endif //MAIN_H

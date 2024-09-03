d3dbd9f3ec0b35501fbcdd8c4a216bc7798d1fa9
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-03 13:48:20 +0200

    Just small adjustments.

--- C Projects/Geeksforgeeks/Rock Paper Scissors/main.h
+++ C Projects/Geeksforgeeks/Rock Paper Scissors/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-bool validInput(char _user_input);
 const char* convertInput(char _player_turn);
+
 uint8_t convertInputToInt( const char* _input);
 uint8_t calculateTheWinner(uint8_t _player_int, uint8_t _bot_int);
+
+bool validInput(char _user_input);
+
 void plotScore(uint8_t _points_player, uint8_t _points_bot);
 
 #endif //MAIN_H

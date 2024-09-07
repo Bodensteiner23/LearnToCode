bbd1af4b76231974e16cb4ec5996d4c39120092d
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-07 12:01:56 +0200

    Simpliefied logic a bit and now abble to debug c projects in vscode.

--- C Projects/Geeksforgeeks/Snake Ladder Game/main.c
+++ C Projects/Geeksforgeeks/Snake Ladder Game/main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 void initBoard(void) {
-    uint8_t board[100];
+    uint8_t board[101];
     bool row_even = true;
 
     for (uint8_t i = 1; i < 101; i++) {
@@ -13,26 +13,26 @@ void initBoard(void) {
     for (uint8_t i = 100; i > 0; i -= 10){
         if (row_even) { // Even rows
             for (uint8_t k = 0; k < 11; k++) {
-                if (((k % 10) == 0) && (k != 0)) {
+                if (k == 10) {
                     printf("\n");
                     row_even = false;
                     continue;
                 }
-                printf("%d  ", board[i - k]);
+                printf("%3d  ", board[i - k]);
             }
         } else {    // Odd rows
             for (int8_t k = 9; k > -1; k--) {
-                if (((k % 10) == 0) && (k != 10)) {
-                    printf("%d  ", board[i]);
+                if (k == 0) {
+                    printf("%3d  ", board[i]);
                     printf("\n");
                     row_even = true;
                     continue;
                 }
                 if (i == 10) {
-                    printf("0%d  ", board[i - k]);
+                    printf("%3d  ", board[i - k]);
                     continue;
                 }
-                printf("%d  ", board[i - k]);
+                printf("%3d  ", board[i - k]);
             }
         }
     }

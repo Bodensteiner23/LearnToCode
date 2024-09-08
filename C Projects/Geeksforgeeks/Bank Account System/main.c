b1a2cebcddc6701c0d99baec2beaa1820708f997
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-08 22:35:37 +0200

    First Version of Specifikation is finished.

--- C Projects/Geeksforgeeks/Bank Account System/main.c
+++ C Projects/Geeksforgeeks/Bank Account System/main.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+/***
+ *  ToDo:
+ *  Programm erstellen das:
+ *  - Nutzer soll sich einloggen können / Account erstellen können. Hierbei prüfen ob
+ *      -> Der Nutzer schon einen Account hat
+ *  - Nutzer soll Geld auf sein Konto einzahlen können, wenn er eingeloggt ist
+ *  - Nutzer soll seinen Kontostand prüfen können
+ *  - Nutzer soll Geld an einen anderen Nutzer überweisen können. Hierbei auch prüfen ob die Person existiert und
+ *  richtig geschrieben wurde
+ ***/
 
 
 
 
-int main(void)
-{
+
+
+int main(void) {
+
+
     return 0;
 }

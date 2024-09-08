2482b057e793ecaa4e68d74fec94e6b436d596ef
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-08 22:52:25 +0200

    Working Version of clearing the screen etc.

--- C Projects/Geeksforgeeks/Bank Account System/main.c
+++ C Projects/Geeksforgeeks/Bank Account System/main.c
@@ -8,14 +8,37 @@
  *  - Nutzer soll Geld an einen anderen Nutzer überweisen können. Hierbei auch prüfen ob die Person existiert und
  *  richtig geschrieben wurde
  ***/
+#include <stdint.h>
+#include <windows.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void goToXY(uint8_t x, uint8_t y) {
+    COORD c;
+    c.X = x;
+    c.Y = y;
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void clearScreen() {
+    system("cls");
+}
+
+
+void starting_screen(void) {
+
+    goToXY(10, 1);
+    printf("Test");
 
 
 
+}
 
 
 
 int main(void) {
-
-
+    clearScreen();
+    starting_screen();
     return 0;
 }

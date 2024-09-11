4223016fd23fcbe8912a5f4b0542f1fe6f138430
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-11 18:57:46 +0200

    For now working verison where the password is still shown.

--- C Projects/Geeksforgeeks/Bank Account System/main.c
+++ C Projects/Geeksforgeeks/Bank Account System/main.c
@@ -3,6 +3,7 @@
  *  Programm erstellen das:
  *  - Nutzer soll sich einloggen können / Account erstellen können. Hierbei prüfen ob
  *      -> Der Nutzer schon einen Account hat
+ *      -> Passwort stark genug ist. Ist quasi noch ein Passwort Prüfer integriert
  *  - Nutzer soll Geld auf sein Konto einzahlen können, wenn er eingeloggt ist
  *  - Nutzer soll seinen Kontostand prüfen können
  *  - Nutzer soll Geld an einen anderen Nutzer überweisen können. Hierbei auch prüfen ob die Person existiert und
@@ -20,6 +21,7 @@ typedef struct {
 } login_data_t;
 
 
+char getPassword(char *_password);
 
 void goToXY(uint8_t x, uint8_t y) {
     COORD c;
@@ -34,7 +36,7 @@ void clearScreen() {
 }
 
 
-int starting_screen(void) {
+int startingScreen(void) {
     goToXY(2, 0);
     printf("******************************");
     goToXY(10, 1);
@@ -55,7 +57,7 @@ int starting_screen(void) {
 }
 
 
-uint8_t create_account(void) {
+uint8_t createAccount(void) {
     login_data_t new_person;
     char buffer[50];
 
@@ -77,21 +79,51 @@ uint8_t create_account(void) {
     scanf("%s", buffer);
     strcpy(new_person.last_name, buffer);
 
+    getPassword(new_person.passwort);
+
+    return 0;
+ 
+}
+
+
+char getPassword(char *_password) {
+    int c = 0;
+    uint8_t i = 0;
+
+    // Clear the input buffer
+    while ((c = getchar()) != '\n');
     
+    goToXY(6, 6);
+    printf("Password  : ");
+    
+    while ((c = getchar()) != '\n' ) {
+        if (c == 10) {          // Enter
+            break;
+        } else if (c == 8) {    // Backspace
+            printf("\b\b");
+        } else {
+            _password[i++] = c;
+        } 
+
+    }
+
+    printf(_password);
 
     return 0;
 
 }
 
 int main(void) {
+    setbuf(stdout, 0);
+
     int user_input = 0;
     // uint8_t operation = 0;
 
     clearScreen();
-    user_input = starting_screen();
+    user_input = startingScreen();
 
     if (user_input == 1) {
-        create_account();
+        createAccount();
     }
     return 0;
 }

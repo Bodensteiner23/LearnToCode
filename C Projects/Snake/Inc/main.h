aae2a1d6133ef2c6ed9aee714353e7d69f06c189
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-09-29 23:26:41 +0200

    Inital Commit of Project.

---
+++ C Projects/Snake/Inc/main.h
@@ -0,0 +1,11 @@
+//
+// Created by User on 29.09.2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdint.h>
+
+int test(void);
+
+#endif //MAIN_H
---
+++ C Projects/Snake/Src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "../Inc/main.h"
+
+
+
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    test();
+    return 0;
+}
+
+
+int test(void) {
+
+    int i = 0;
+
+    // return i;
+
+}

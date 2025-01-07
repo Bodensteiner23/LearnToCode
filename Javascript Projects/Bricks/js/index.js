3cad77870b2ed2c289329c89741618ed1ca2869b
Bodensteiner23 <matthias.bodensteiner@fau.de>
2025-01-07 10:55:46 +0100

    Added start game button.

--- Javascript Projects/Bricks/js/index.js
+++ Javascript Projects/Bricks/js/index.js
@@ -1,7 +1,15 @@
+window.addEventListener("load", (event) => {
+  testFunction();
+});
+
 function testFunction() {
   const playingField = document.getElementById("playingField");
   const para = document.createElement("h1");
   para.textContent = "Test";
   playingField.appendChild(para);
   console.log("test");
+}
+
+function startGame() {
+  return true;
 }

d889b799dd8b227bb377667046551bc5f204fd79
Matthias Bodensteiner <matthias.bodensteiner@boardforge.io>
2025-04-22 11:13:59 +0200

    Restarted.

--- c++/2_anlage/classes/DI_0_24V_Input.h
+++ c++/2_anlage/classes/DI_0_24V_Input.h
@@ -4,13 +4,5 @@
 
 class DI_0_24V_Input {
 private:
-    bool input_state;
-    std::string input_name;     // X11_1, X11_2 ...
-    // Oder einfach ID von jeder Nachricht hinzufügen statt name / Zusätzlich?
 public:
-    DI_0_24V_Input(const std::string& name) : input_name(name), input_state(false){}
-    bool get_value() const{
-        return input_state;
-        // Hier wird von Tabelle gelesen oder die Kommunikationsklasse aufgerufen?
-    }
 };

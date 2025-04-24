53c5e42ad40840c48b9dea471c1c90394cf7e77e
Matthias Bodensteiner <matthias.bodensteiner@boardforge.io>
2025-04-24 17:54:08 +0200

    Refactor Book class and add console menu for library system

--- c++/4_library_management_system/main.cpp
+++ c++/4_library_management_system/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+#include <vector>
 
 struct BookData {
     std::string book_name;
     std::string autor_name;
     int publication_year;
-    int reading_date;
-    int book_note;
+    int reading_year;
+    int book_grade;
 };
 
 
 class Book {
     BookData data;
 public:
-    Book(const std::string &name, const std::string &autor, int year, int read_date, int note);
+    Book(const std::string &name, const std::string &autor, int pub_year, int read_year, int grade);
     // Getters
     BookData get_book_data();
 };
 
-Book::Book(const std::string &name, const std::string &autor, const int year, const int read_date, const int note)
-            : data(name, autor, year, read_date, note) {};
+Book::Book(const std::string &name, const std::string &autor, const int pub_year, const int read_year, const int grade)
+            : data(name, autor, pub_year, read_year, grade) {};
 
 BookData Book::get_book_data() {
     return data;
@@ -26,15 +27,88 @@ BookData Book::get_book_data() {
 
 
 
-int main() {
-    Book book1 ("Buch", "Autor", 2000, 2025, 8);
-    BookData book_data = book1.get_book_data();
+namespace Console {
+    void show_menue() {
+        std::cout << "----------- Library System -----------" << std::endl;
+        std::cout << "1. Add Book"      << std::endl;
+        std::cout << "2. Show Books"    << std::endl;
+        std::cout << "3. Delete Book"   << std::endl;
+        std::cout << "--------------------------------------" << std::endl;
+    }
 
-    std::cout << "Book Name: " << book_data.book_name << std::endl;
-    std::cout << "Autor Name: " << book_data.autor_name << std::endl;
-    std::cout << "Pub Year: " << book_data.publication_year << std::endl;
-    std::cout << "Read Date: " << book_data.reading_date << std::endl;
-    std::cout << "Book Note: " << book_data.book_note << std::endl;
+    BookData add_book_menue() {
+        BookData data;
+        std::cout << "Book name: ";
+        std::cin >> data.book_name;
+        std::cout << "Autor name: ";
+        std::cin >> data.autor_name;
+        std::cout << "Publication year: ";
+        std::cin >> data.publication_year;
+        std::cout << "Reading year: ";
+        std::cin >> data.reading_year;
+        std::cout << "Book grade: ";
+        std::cin >> data.book_grade;
+
+        return data;
+    }
+
+    void print_book_data(const BookData &data) {
+        std::cout << "Book name: " << data.book_name << std::endl;
+        std::cout << "Autor name: " << data.autor_name << std::endl;
+        std::cout << "Publication year: " << data.publication_year << std::endl;
+        std::cout << "Reading year: " << data.reading_year << std::endl;
+        std::cout << "Book grade: " << data.book_grade << std::endl;
+    }
+
+    void print_book_list(std::vector<Book> booklist) {
+        for (size_t i = 0; i < booklist.size(); i++) {
+            std::cout << "# Book " << i + 1 << " ----------------- #" << std::endl;
+            print_book_data(booklist[i].get_book_data());
+        }
+    }
+
+    char get_input() {
+        while (true) {
+            char user_input;
+            std::cout << "Input: ";
+            std::cin >> user_input;
+            // Compare with ASCII Table
+            if (user_input >= 48 && user_input <= 51 ) {
+                return user_input;
+            }
+            std::cout << "Invalid Input. Try again!" << std::endl;
+        }
+    }
+
+}
+
+
+int main() {
+
+    std::vector<Book> BookList;
+
+    // Console::show_menue();
+    // char type = Console::get_input();
+    // BookData data = Console::add_book_menue();
+    // std::cout << data.autor_name;
+
+
+    Book book1 ("Buch", "Autor", 2000, 2025, 8);
+    BookList.push_back(book1);
+    Book book2 ("Autor", "Buch", 2000, 2025, 8);
+    BookList.push_back(book2);
+
+    Console::print_book_list(BookList);
+
+
+
+    // BookData book_data = book1.get_book_data();
+    //
+    // std::cout << "Book Name: " << book_data.book_name << std::endl;
+    // std::cout << "Autor Name: " << book_data.autor_name << std::endl;
+    // std::cout << "Pub Year: " << book_data.publication_year << std::endl;
+    // std::cout << "Read Date: " << book_data.reading_date << std::endl;
+    // std::cout << "Book Note: " << book_data.book_note << std::endl;
 
 
     return 0;

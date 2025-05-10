29c846a2e73ef6b8929eb99f0baeee1448806c15
Matthias Bodensteiner <matthias.bodensteiner@boardforge.io>
2025-05-10 15:33:07 +0200

    Created playing field

--- c++/games/tictactoe/main.cpp
+++ c++/games/tictactoe/main.cpp
@@ -7,29 +7,33 @@ public:
     UI(sf::RenderWindow& window) : window_ui(window) {}
 
     void draw_playing_field() {
-        sf::RectangleShape line1({150, 10});
-        line1.setPosition({100, 100});
-        line1.setFillColor(sf::Color::Red);
-        line1.setRotation(sf::degrees(90));
-        window_ui.draw(line1);
-        sf::RectangleShape line2({150, 10});
-        line2.setPosition({300, 100});
-        line2.setFillColor(sf::Color::Red);
-        line2.setRotation(sf::degrees(90));
-        window_ui.draw(line2);
+        sf::RectangleShape line_horiz_1({600, 5});
+        line_horiz_1.setPosition({100, 300});
+        line_horiz_1.setFillColor(sf::Color::Red);
+        window_ui.draw(line_horiz_1);
+        sf::RectangleShape line_horiz_2({600, 5});
+        line_horiz_2.setPosition({100, 500});
+        line_horiz_2.setFillColor(sf::Color::Red);
+        window_ui.draw(line_horiz_2);
 
-
-
-
+        sf::RectangleShape line_vert_1({600, 5});
+        line_vert_1.setPosition({300, 100});
+        line_vert_1.setFillColor(sf::Color::Red);
+        line_vert_1.rotate(sf::degrees(90));
+        window_ui.draw(line_vert_1);
+        sf::RectangleShape line_vert_2({600, 5});
+        line_vert_2.setPosition({500, 100});
+        line_vert_2.setFillColor(sf::Color::Red);
+        line_vert_2.rotate(sf::degrees(90));
+        window_ui.draw(line_vert_2);
     }
-
 };
 
 
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    sf::RenderWindow window(sf::VideoMode({800, 800}), "TicTacToe");
     UI ui(window);
     while (window.isOpen())
     {
@@ -40,7 +44,6 @@ int main()
         }
 
         window.clear();
-
         ui.draw_playing_field();
 
         window.display();

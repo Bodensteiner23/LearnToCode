cb775c8b1dedad234a50da3c6c3f77ec393a6912
Matthias Bodensteiner <matthias.bodensteiner@boardforge.io>
2025-05-29 22:48:42 +0200

    Refactor UI and APP classes to improve grid drawing and square click handling

--- c++/games/tictactoe/main.cpp
+++ c++/games/tictactoe/main.cpp
@@ -2,13 +2,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#define PLAYER_1 1
+#define PLAYER_1_SYMBOL 1
+#define PLAYER_2 2
+#define PLAYER_2_SYMBOL 2
+
+uint8_t current_player_symbol = 1;
+
 class UI {
     sf::RenderWindow& window_ui;
     std::array<sf::RectangleShape, 9> squares;
     int square_array[9] = { 0 };
     const float line_thickness = 5;
 
-    void draw_grid_rectangle(sf::Vector2f size, sf::Vector2f position , sf::Color color) {
+    void draw_rectangle(sf::Vector2f size, sf::Vector2f position , sf::Color color) {
         sf::RectangleShape rectangle(size);
         rectangle.setPosition(position);
         rectangle.setFillColor(color);
@@ -20,6 +27,14 @@ class UI {
         squares[index].setPosition(position);
         squares[index].setFillColor(color);
         window_ui.draw(squares[index]);
+    }
+
+    void draw_grid() {
+        sf::Color color = sf::Color::White;
+        draw_rectangle({600, 5}, {100, 300}, color);
+        draw_rectangle({600, 5}, {100, 500}, color);
+        draw_rectangle({5, 600}, {300, 100}, color);
+        draw_rectangle({5, 600}, {500, 100}, color);
     }
 
     void draw_squares() {
@@ -40,43 +55,48 @@ public:
 
     void draw_playing_field() {
         window_ui.clear(sf::Color(81, 85, 92));
-        // draw the grid
-        draw_grid_rectangle({600, 5}, {100, 300}, sf::Color::White);
-        draw_grid_rectangle({600, 5}, {100, 500}, sf::Color::White);
-        draw_grid_rectangle({5, 600}, {300, 100}, sf::Color::White);
-        draw_grid_rectangle({5, 600}, {500, 100}, sf::Color::White);
-
+        draw_grid();
         draw_squares();
     }
 
-    void check_square_clicked(sf::Vector2i mousePos) {
-        for (size_t i = 0; i < squares.size(); i++) {
+    int get_clicked_square(sf::Vector2i mousePos) {
+        for (int i = 0; i < squares.size(); i++) {
             if (squares[i].getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))) {
-                square_array[i] = 1;
-                std::cout << "Square " << i << " clicked!" << std::endl;
-                break;
+                return i;
             }
         }
+        return -1;
     }
 
-    int get_square_array_value(int index) {
-        return square_array[index];
-    }
 };
 
 
 class APP {
-
+    int square_array[9] = { 0 };
 
 public:
+    int get_square_array_value(int index) {
+        return square_array[index];
+    }
+
+    void update_square_array(int index) {
+        square_array[index] = current_player_symbol;
+        if (current_player_symbol == PLAYER_1) {
+            current_player_symbol = PLAYER_2;
+        } else {
+            current_player_symbol = PLAYER_1;
+        }
+
+    }
+
 };
 
 
-int main()
-{
-
+int main() {
     sf::RenderWindow window(sf::VideoMode({800, 800}), "TicTacToe");
     UI ui(window);
+    APP app;
+
 
     while (window.isOpen())
     {
@@ -84,6 +104,18 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            if (const auto* mouse_button_pressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+                if (mouse_button_pressed->button == sf::Mouse::Button::Left) {
+                    // Handle mouse click
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    int clicked_square = ui.get_clicked_square(mousePos);
+                    if (clicked_square != -1) {
+                        app.update_square_array(clicked_square);
+                    }
+
+                }
+            }
         }
 
         window.clear();
@@ -92,18 +124,16 @@ int main()
 
         window.display();
 
-        while (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
-        {
-
-            // Handle mouse click
-            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-            ui.check_square_clicked(mousePos);
-            //delay
-            sf::sleep(sf::milliseconds(200));
-
-
-        }
-
-
+        // while (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        // {
+        //     // Handle mouse click
+        //     sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        //     ui.get_clicked_square(mousePos);
+        //     //delay
+        //     sf::sleep(sf::milliseconds(200));
+        //
+        //
+        // }
     }
+
 }
